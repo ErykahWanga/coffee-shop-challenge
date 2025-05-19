1e21b09a56f593380a69695733ecee1bd266b649
Erykah <erkahwanga@gmail.com>
2025-05-19 08:12:24 +0300

    Implement customer order management methods and most aficionado calculation

--- customer.py
+++ customer.py
@@ -17,4 +17,21 @@ class Customer:
             raise ValueError("Name must be 1–15 characters")
         self._name = value
 
-    
+    def orders(self):
+        return [order for order in Order._all if order.customer == self]
+
+    def coffees(self):
+        return list(set(order.coffee for order in self.orders()))
+
+    def create_order(self, coffee, price):
+        return Order(self, coffee, price)
+
+    @classmethod
+    def most_aficionado(cls, coffee):
+        if not coffee.orders():
+            return None
+        customer_spending = {}
+        for order in coffee.orders():
+            customer = order.customer
+            customer_spending[customer] = customer_spending.get(customer, 0) + order.price
+        return max(customer_spending.items(), key=lambda x: x[1])[0]
