a3a6a9512b51dddfc86163c694f4ab87e4ece476
Erykah <erkahwanga@gmail.com>
2025-05-19 08:15:49 +0300

    Add methods for order management and customer retrieval in Coffee class

--- coffee.py
+++ coffee.py
@@ -6,4 +6,19 @@ class Coffee:
             raise ValueError("Name must be at least 3 characters")
         self._name = name
 
-   
+    @property
+    def name(self):
+        return self._name
+
+    def orders(self):
+        return [order for order in Order._all if order.coffee == self]
+
+    def customers(self):
+        return list(set(order.customer for order in self.orders()))
+
+    def num_orders(self):
+        return len(self.orders())
+
+    def average_price(self):
+        orders = self.orders()
+        return sum(order.price for order in orders) / len(orders) if orders else 0
