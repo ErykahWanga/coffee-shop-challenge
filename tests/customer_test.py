3d1116260a1e81cee4633bbe05aa1d1a00a5d6b5
Erykah <erkahwanga@gmail.com>
2025-05-19 08:25:33 +0300

    Add additional tests for Customer class methods in customer_test.py

--- tests/customer_test.py
+++ tests/customer_test.py
@@ -17,4 +17,25 @@ class TestCustomer(unittest.TestCase):
         with self.assertRaises(ValueError):
             Customer("A" * 16)
 
-    
+    def test_orders(self):
+        order = Order(self.customer, self.coffee, 5.0)
+        self.assertEqual(self.customer.orders(), [order])
+
+    def test_coffees(self):
+        order = Order(self.customer, self.coffee, 5.0)
+        self.assertEqual(self.customer.coffees(), [self.coffee])
+
+    def test_create_order(self):
+        order = self.customer.create_order(self.coffee, 5.0)
+        self.assertEqual(order.customer, self.customer)
+        self.assertEqual(order.coffee, self.coffee)
+        self.assertEqual(order.price, 5.0)
+
+    def test_most_aficionado(self):
+        customer2 = Customer("Bob")
+        Order(self.customer, self.coffee, 5.0)
+        Order(customer2, self.coffee, 7.0)
+        self.assertEqual(Customer.most_aficionado(self.coffee), customer2)
+
+if __name__ == "__main__":
+    unittest.main()
