c69d2130f819cfdeea84d97e35153548672d021d
Erykah <erkahwanga@gmail.com>
2025-05-19 08:18:09 +0300

    Refactor Order class to ensure property methods are defined after initialization

--- order.py
+++ order.py
@@ -14,3 +14,15 @@ class Order:
         self._coffee = coffee
         self._price = price
         self._all.append(self)
+
+    @property
+    def price(self):
+        return self._price
+
+    @property
+    def customer(self):
+        return self._customer
+
+    @property
+    def coffee(self):
+        return self._coffee
